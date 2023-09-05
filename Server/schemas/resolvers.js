3d453a756c8617d2fbb193138bc461c7f1fe39d7
linuscth <1020cheung@gmail.com>
2023-09-05 00:00:54 -0700

    first commit

---
+++ Server/schemas/resolvers.js
@@ -0,0 +1,66 @@
+const { User } = require('../models')
+const { signToken, AuthenticationError } = require('../utils/auth')
+
+const resolvers = {
+    Query: {
+        Me: async (parents, args, context) => {
+            if (context.user) {
+                return User.findOne({ _id: context.user._id })
+            }
+            throw AuthenticationError
+        }
+    },
+
+    Mutation: {
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne(email);
+            if (!user) {
+                throw AuthenticationError
+            }
+            const correctPw = await user.isCorrectPassword(password)
+
+            if (!correctPw) {
+                throw AuthenticationError
+            }
+            const token = signToken(user);
+            return { token, user }
+        },
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+            return { token, user }
+        },
+        saveBook: async (parent, { bookInput }, context) => {
+            try {
+                if (!context.user) {
+                    throw AuthenticationError
+                }
+                const user = await User.findOneAndUpdate({ _id: context.user._id }, {
+                    $addToSet: { savedBooks: bookInput }
+                }, { new: true, runValidators: true }
+                )
+
+
+            } catch (error) {
+                console.log(error);
+                throw AuthenticationError
+            }
+        },
+        removeBook: async (parent, { bookId }, context) => {
+            try {
+                if (!context.user) {
+                    throw AuthenticationError
+                }
+                const user = await User.findOneAndUpdate({ _id: context.user._id }, {
+                    $pull: { savedBooks: { bookId } }
+                }, { new: true })
+            } catch (error) {
+                console.log(error);
+                throw AuthenticationError
+            }
+        }
+
+    }
+}
+
+module.exports = resolvers
---
+++ Server/schemas/typeDefs.js
@@ -0,0 +1,42 @@
+const typeDefs = `
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    password: String!
+    savedBooks: [Book]
+  }
+  type Book {
+    _id: ID!
+    authors: [String]
+    description: String!
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+  }
+  type Auth {
+    token: ID!
+    user: User
+  }
+input BookInput {
+    authors: [String]
+    description: String!
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+}
+  type Query {
+   Me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookinput: BookInput!): User
+    removeBook(bookId: String!): User
+  }
+`;
+
+module.exports = typeDefs;
